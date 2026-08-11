cc77542a29c2df0e95e0aaae35f208d83a845020
Soda <km7788@proton.me>
2026-08-11 11:42:50 +0800

    Sync phase file names with renumbered pipeline: screenwriter->phase1, storyboard->phase2, video/consistency/assembly/post shifted to phase6-9; remove import_module concat hack

--- pipeline/src/phases/__init__.py
+++ pipeline/src/phases/__init__.py
@@ -1,14 +1,12 @@
 """Pipeline phase entry points."""
 
-from importlib import import_module
-
 from .phase1_director import run_phase1
-from .phase2_screenwriter import run_phase2
-run_phase2_storyboard = import_module(".phase2" + "_5_storyboard", __name__).run_phase2_storyboard
+from .phase1_screenwriter import run_phase2
+from .phase2_storyboard import run_phase2_storyboard
 from .phase3_character import run_phase3
 from .phase4_orchestrator import run_phase4
 from .storyboard_qa_gate import run_storyboard_qa_gate
-from .phase5_video_gen import run_phase5
-from .phase6_consistency import run_phase6
-from .phase7_assembly import run_phase7
-from .phase8_post import run_phase8
+from .phase6_video_gen import run_phase5
+from .phase7_consistency import run_phase6
+from .phase8_assembly import run_phase7
+from .phase9_post import run_phase8
